223b70ab9652def32e49246b179c9b25fb8020f7
ZeaR29 <112360432+Drkg29@users.noreply.github.com>
2024-05-22 14:44:06 -0500

    Add files via upload

---
+++ test/java/com/example/suplementos/suplementosstores/SuplementosstoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suplementos.suplementosstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuplementosstoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
